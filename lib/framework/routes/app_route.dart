1ece527061101a9302081cc9faaf18d6ca94fb9a
kurniawan <kurni.aawann19@gmail.com>
2024-09-28 21:16:40 +0700

    add route

---
+++ lib/framework/routes/app_route.dart
@@ -0,0 +1,9 @@
+part of 'app_pages.dart';
+
+abstract class Routes {
+  static const home = 'home';
+  static const product = 'product';
+  static const service = 'service';
+  static const history = 'history';
+  static const account = 'account';
+}

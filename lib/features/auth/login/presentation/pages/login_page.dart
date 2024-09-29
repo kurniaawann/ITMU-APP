fb899fb87a9eac9bb9c7f1f6a26a95fd553910a4
kurniawan <kurni.aawann19@gmail.com>
2024-09-29 15:54:37 +0700

    change login page

--- lib/features/auth/login/presentation/pages/login_page.dart
+++ lib/features/auth/login/presentation/pages/login_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_itmu/features/auth/login/presentation/bloc/login_bloc.dart';
 import 'package:mobile_itmu/features/auth/login/presentation/widgets/login_body.dart';
+import 'package:mobile_itmu/service_locator.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const LoginBody();
+    return BlocProvider(
+      create: (context) => serviceLocator<LoginBloc>(),
+      child: Scaffold(
+        appBar: AppBar(),
+        body: const LoginBody(),
+      ),
+    );
   }
 }
